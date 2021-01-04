31474f16b666dc5bb9b9c569f37bfb110797531a
yvs1364 <yvs.pro64@gmail.com>
2021-01-04 09:46:53 +0100

    for loop

---
+++ for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int start(1);
+    int ending(100);
+
+    for(start; start <= ending; start++ ){
+        cout << start << endl;
+    }
+}
